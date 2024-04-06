4b3e03d3aa72252d1fae74df0be58b0ccb034736
Sampath_Kumara <84168369+sampath-kumaramd@users.noreply.github.com>
2024-04-06 19:09:53 +0530

    format component

--- components/header.tsx
+++ components/header.tsx
@@ -19,43 +19,41 @@ function Header() {
     const form = useForm()
     return (
         <>
-        <div className="bg-primary h-24 flex items-center">
-            <div className=' mx-32 flex justify-between h-auto w-full'>
-                <div className=" flex gap-8">
-                    <Image src='/icons/logo.png' alt='logo' width={200} height={42} />
-                    <DropdownMenu>
-                        <DropdownMenuTrigger className=" text-white focus:border-none">
-                            <div className="flex gap-2">Categorias <Icons.ChevronDown /></div></DropdownMenuTrigger>
-                        <DropdownMenuContent>
-                            <DropdownMenuLabel>Categorias</DropdownMenuLabel>
-                            <DropdownMenuSeparator />
-                            <DropdownMenuItem>Profile</DropdownMenuItem>
-                            <DropdownMenuItem>Billing</DropdownMenuItem>
-                            <DropdownMenuItem>Team</DropdownMenuItem>
-                            <DropdownMenuItem>Subscription</DropdownMenuItem>
-                        </DropdownMenuContent>
-                    </DropdownMenu>
+            <div className="bg-primary h-24 flex items-center">
+                <div className=' mx-32 flex justify-between h-auto w-full'>
+                    <div className=" flex gap-8">
+                        <Image src='/icons/logo.png' alt='logo' width={200} height={42} />
+                        <DropdownMenu>
+                            <DropdownMenuTrigger className=" text-white focus:border-none">
+                                <div className="flex gap-2">Categorias <Icons.ChevronDown /></div></DropdownMenuTrigger>
+                            <DropdownMenuContent>
+                                <DropdownMenuLabel>Categorias</DropdownMenuLabel>
+                                <DropdownMenuSeparator />
+                                <DropdownMenuItem>Profile</DropdownMenuItem>
+                                <DropdownMenuItem>Billing</DropdownMenuItem>
+                                <DropdownMenuItem>Team</DropdownMenuItem>
+                                <DropdownMenuItem>Subscription</DropdownMenuItem>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
 
-                </div>
-                <div>
-                    <Input placeholder="O que está procurando? " className=" rounded-full w-96" />
-                </div>
-                <div className="flex gap-4">
-                    <Button className="flex gap-2 bg-[#d26841] hover:bg-[#a5423b] rounded-3xl border-white border-2">
-                        <Image src='/icons/telegram.png' alt='logo' width={30} height={30} />
-                        <span className="text-white">Telegram</span>
-                    </Button>
-                    <Button className="flex gap-2 bg-[#d26841] hover:bg-[#a5423b] rounded-3xl border-white border-2">
-                        <Image src='/icons/whatsapp.png' alt='logo' width={30} height={30} />
-                        <span className="text-white">Whatsapp</span>
-                    </Button>
+                    </div>
+                    <div>
+                        <Input placeholder="O que está procurando? " className=" rounded-full w-96" />
+                    </div>
+                    <div className="flex gap-4">
+                        <Button className="flex gap-2 bg-[#d26841] hover:bg-[#a5423b] rounded-3xl border-white border-2">
+                            <Image src='/icons/telegram.png' alt='logo' width={30} height={30} />
+                            <span className="text-white">Telegram</span>
+                        </Button>
+                        <Button className="flex gap-2 bg-[#d26841] hover:bg-[#a5423b] rounded-3xl border-white border-2">
+                            <Image src='/icons/whatsapp.png' alt='logo' width={30} height={30} />
+                            <span className="text-white">Whatsapp</span>
+                        </Button>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div className="h-1 bg-[#eacb3d]">
-
-        </div>
-
+            <div className="h-1 bg-[#eacb3d]">
+            </div>
         </>
     )
 }
